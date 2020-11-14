f70dc9f10326177fc5015f2ccb3716cfa2718f08
Iancrix <ianchonaramirez@gmail.com>
2020-11-14 07:06:48 -0500

    lol

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
-// Server Init
+// Server Init lol
 app.listen(PORT/*, HOST*/, () => console.log(`Server started on port ${PORT}`));
